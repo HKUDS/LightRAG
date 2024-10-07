fba232487b94c568e52504e4a9a5da14d0448d9f
Larfii <834462287@qq.com>
2024-10-07 18:48:45 +0800

    update

---
+++ examples/insert.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append('xxx/xxx/LightRAG')
+
+from lightrag import LightRAG
+
+os.environ["OPENAI_API_KEY"] = ""
+
+WORKING_DIR = ""
+
+if not os.path.exists(WORKING_DIR):
+    os.mkdir(WORKING_DIR)
+
+rag = LightRAG(working_dir=WORKING_DIR)
+
+with open('./text.txt', 'r') as f:
+    text = f.read()
+
+rag.insert(text)
