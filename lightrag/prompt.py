e6e6afa81e4a3894433a9bc87144025a3869adde
zrguo <49157727+LarFii@users.noreply.github.com>
2024-10-19 11:46:03 +0800

    fix prompt.py

--- lightrag/prompt.py
+++ lightrag/prompt.py
@@ -163,24 +163,9 @@ Do not include information where the supporting evidence for it is not provided.
 
 {response_type}
 
-
 ---Data tables---
 
 {context_data}
-
-
----Goal---
-
-Generate a response of the target length and format that responds to the user's question, summarizing all information in the input data tables appropriate for the response length and format, and incorporating any relevant general knowledge.
-
-If you don't know the answer, just say so. Do not make anything up.
-
-Do not include information where the supporting evidence for it is not provided.
-
-
----Target response length and format---
-
-{response_type}
 
 Add sections and commentary to the response as appropriate for the length and format. Style the response in markdown.
 """
