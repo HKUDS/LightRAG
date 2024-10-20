657c4aaaac506fe216f223ceeec9d456c05b1abf
nongbin <bin_nong@foxmail.com>
2024-10-20 21:17:09 +0800

    make graph visualization become colorful

--- examples/graph_visual.py
+++ examples/graph_visual.py
@@ -1,5 +1,6 @@
 import networkx as nx
 from pyvis.network import Network
+import random
 
 # Load the GraphML file
 G = nx.read_graphml('./dickens/graph_chunk_entity_relation.graphml')
@@ -9,6 +10,10 @@ net = Network(notebook=True)
 
 # Convert NetworkX graph to Pyvis network
 net.from_nx(G)
+
+# Add colors to nodes
+for node in net.nodes:
+    node['color'] = "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
 # Save and display the network
 net.show('knowledge_graph.html')
