e07012767dd00bf03e64c03ff577499c529144a4
LarFii <834462287@qq.com>
2024-10-20 23:10:07 +0800

    Update __init__.py

--- lightrag/__init__.py
+++ lightrag/__init__.py
@@ -1,5 +1,5 @@
 from .lightrag import LightRAG as LightRAG, QueryParam as QueryParam
 
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __author__ = "Zirui Guo"
 __url__ = "https://github.com/HKUDS/LightRAG"
