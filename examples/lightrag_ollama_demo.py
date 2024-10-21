6941f3e30942d1215991de2ac5907ecc3ae5ff39
Andrii Lazarchuk <alazarchuk@gmail.com>
2024-10-21 11:53:06 +0000

    Add ability to passadditional parameters to ollama library like host and timeout

--- examples/lightrag_ollama_demo.py
+++ examples/lightrag_ollama_demo.py
@@ -1,4 +1,7 @@
 import os
+import logging
+
+logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.DEBUG)
 
 from lightrag import LightRAG, QueryParam
 from lightrag.llm import ollama_model_complete, ollama_embedding
@@ -11,15 +14,17 @@ if not os.path.exists(WORKING_DIR):
 
 rag = LightRAG(
     working_dir=WORKING_DIR,
-    llm_model_func=ollama_model_complete,  
-    llm_model_name='your_model_name',
+    tiktoken_model_name="mistral:7b",
+    llm_model_func=ollama_model_complete,
+    llm_model_name="mistral:7b",
+    llm_model_max_async=2,
+    llm_model_kwargs={"host": "http://localhost:11434"},
     embedding_func=EmbeddingFunc(
         embedding_dim=768,
         max_token_size=8192,
         func=lambda texts: ollama_embedding(
-            texts, 
-            embed_model="nomic-embed-text"
-        )
+            texts, embed_model="nomic-embed-text", host="http://localhost:11434"
+        ),
     ),
 )
 
@@ -28,13 +33,21 @@ with open("./book.txt") as f:
     rag.insert(f.read())
 
 # Perform naive search
-print(rag.query("What are the top themes in this story?", param=QueryParam(mode="naive")))
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="naive"))
+)
 
 # Perform local search
-print(rag.query("What are the top themes in this story?", param=QueryParam(mode="local")))
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="local"))
+)
 
 # Perform global search
-print(rag.query("What are the top themes in this story?", param=QueryParam(mode="global")))
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="global"))
+)
 
 # Perform hybrid search
-print(rag.query("What are the top themes in this story?", param=QueryParam(mode="hybrid")))
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="hybrid"))
+)
