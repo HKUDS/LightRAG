83865a099ebad8cc329d0c6f00cb2723f34bdd33
Paxwell-Paxwell <pompom231245@gmail.com>
2024-10-25 00:05:03 +0700

    fix evenloop and add neo4j

---
+++ examples/lightrag_openai_demo_storage.py
@@ -0,0 +1,39 @@
+import os
+
+from lightrag import LightRAG, QueryParam
+from lightrag.llm import gpt_4o_mini_complete
+from lightrag.storage import Neo4jKVStorage
+
+rag = LightRAG (
+    working_dir="./dickens",
+    llm_model_func=gpt_4o_mini_complete,
+    neo4j_config={
+        "uri": "neo4j://localhost:7687",
+        "username": "neo4j",
+        "password": "admin12345"
+    },
+)
+
+
+with open("./book.txt", "r", encoding="utf-8") as f:
+    rag.insert(f.read())
+
+# Perform naive search
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="naive"))
+)
+
+# Perform local search
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="local"))
+)
+
+# Perform global search
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="global"))
+)
+
+# Perform hybrid search
+print(
+    rag.query("What are the top themes in this story?", param=QueryParam(mode="hybrid"))
+)
