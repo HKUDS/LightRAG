1af47ab26886629a5803afdacc33e82e23a2ba51
zrguo <49157727+LarFii@users.noreply.github.com>
2024-10-26 14:04:11 +0800

    Update graph_visual_with_html.py

--- examples/graph_visual_with_html.py
+++ examples/graph_visual_with_html.py
@@ -6,7 +6,7 @@ import random
 G = nx.read_graphml("./dickens/graph_chunk_entity_relation.graphml")
 
 # Create a Pyvis network
-net = Network(notebook=True)
+net = Network(height="100vh", notebook=True)
 
 # Convert NetworkX graph to Pyvis network
 net.from_nx(G)
