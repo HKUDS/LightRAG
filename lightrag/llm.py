8b8617a33c5e5879c3d6a91d3e0b97a42e79339a
yusufyesil <62959716+yusufyesil@users.noreply.github.com>
2024-11-02 22:51:08 +0300

    Update llm.py

--- lightrag/llm.py
+++ lightrag/llm.py
@@ -40,51 +40,7 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
     retry=retry_if_exception_type((RateLimitError, APIConnectionError, Timeout)),
 )
 
-"""
-async def openai_complete_if_cache(
-    model,
-    prompt,
-    system_prompt=None,
-    history_messages=[],
-    base_url=None,
-    api_key=None,
-    **kwargs,
-) -> str:
-    if api_key:
-        os.environ["OPENAI_API_KEY"] = api_key
 
-    openai_async_client = (
-        AsyncOpenAI() if base_url is None else AsyncOpenAI(base_url=base_url)
-    )
-    hashing_kv: BaseKVStorage = kwargs.pop("hashing_kv", None)
-    messages = []
-    if system_prompt:
-        messages.append({"role": "system", "content": system_prompt})
-    messages.extend(history_messages)
-    messages.append({"role": "user", "content": prompt})
-    if hashing_kv is not None:
-        args_hash = compute_args_hash(model, messages)
-        if_cache_return = await hashing_kv.get_by_id(args_hash)
-        if if_cache_return is not None:
-            return if_cache_return["return"]
-
-    response = await openai_async_client.chat.completions.create(
-        model=model, messages=messages, **kwargs
-    )
-
-    if hashing_kv is not None:
-        await hashing_kv.upsert(
-            {args_hash: {"return": response.choices[0].message.content, "model": model}}
-        )
-    return response.choices[0].message.content
-
-
-@retry(
-    stop=stop_after_attempt(3),
-    wait=wait_exponential(multiplier=1, min=4, max=10),
-    retry=retry_if_exception_type((RateLimitError, APIConnectionError, Timeout)),
-)
-"""
 
 
 async def openai_complete_if_cache(
@@ -520,18 +476,6 @@ async def gpt_4o_complete(
     )
 
 
-"""
-async def gpt_4o_mini_complete(
-    prompt, system_prompt=None, history_messages=[], **kwargs
-) -> str:
-    return await model.generate_content(
-        "gpt-4o-mini",
-        prompt,
-        system_prompt=system_prompt,
-        history_messages=history_messages,
-        **kwargs,
-    )
-"""
 
 async def gpt_4o_mini_complete(
     prompt, system_prompt=None, history_messages=[], **kwargs
