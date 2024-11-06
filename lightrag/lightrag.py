8420cd1c77cab5c42ccc3bc8f98cf4289827f5d8
Ken Wiltshire <wiltshirek@gmail.com>
2024-11-06 10:49:14 -0500

    event loop issue

--- lightrag/lightrag.py
+++ lightrag/lightrag.py
@@ -52,15 +52,13 @@ from .base import (
     QueryParam,
 )
 
-
 def always_get_an_event_loop() -> asyncio.AbstractEventLoop:
     try:
-        loop = asyncio.get_running_loop()
+        loop = asyncio.get_event_loop()
     except RuntimeError:
         logger.info("Creating a new event loop in main thread.")
-        # loop = asyncio.new_event_loop()
-        # asyncio.set_event_loop(loop)
-        loop = asyncio.get_event_loop()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
     return loop
 
 
