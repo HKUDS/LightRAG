fd50c3a240dddc2cd6d11adcf69a40094ea61a47
yangdx <yangdx@znipower.com>
2025-01-15 17:43:00 +0800

    修改流处理逻辑，修改 /api/tags错误

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,1 @@
-lightrag-server --llm-binding openai --llm-model deepseek-chat --embedding-model "bge-m3:latest" --embedding-dim 1024
+lightrag-ollama --llm-binding openai --llm-model deepseek-chat --embedding-model "bge-m3:latest" --embedding-dim 1024
