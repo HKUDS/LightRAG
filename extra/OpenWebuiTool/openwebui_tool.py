2dfbbec4073aee262cb409c3bf9283f08506d399
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2025-01-19 23:25:26 +0100

    Started a dummy OpenWebUI tool. I will upload the working version in the next pull request

---
+++ extra/OpenWebuiTool/openwebui_tool.py
@@ -0,0 +1,34 @@
+"""
+OpenWebui Lightrag Integration Tool
+==================================
+
+This tool enables the integration and use of Lightrag within the OpenWebui environment,
+providing a seamless interface for RAG (Retrieval-Augmented Generation) operations.
+
+Author: ParisNeo (parisneoai@gmail.com)
+Social:
+    - Twitter: @ParisNeo_AI
+    - Reddit: r/lollms
+    - Instagram: https://www.instagram.com/parisneo_ai/
+
+License: Apache 2.0
+Copyright (c) 2024-2025 ParisNeo
+
+This tool is part of the LoLLMs project (Lord of Large Language and Multimodal Systems).
+For more information, visit: https://github.com/ParisNeo/lollms
+
+Requirements:
+    - Python 3.8+
+    - OpenWebui
+    - Lightrag
+"""
+
+from pathlib import Path
+from typing import Optional, List, Dict, Union, Any
+from datetime import datetime
+
+# Tool version
+__version__ = "1.0.0"
+__author__ = "ParisNeo"
+__author_email__ = "parisneoai@gmail.com"
+__description__ = "Lightrag integration for OpenWebui"
