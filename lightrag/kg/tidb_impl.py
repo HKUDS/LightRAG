524295ddf752167ac0caab6e0dbb49fc19f97b8f
yangdx <yangdx@znipower.com>
2025-02-11 04:27:45 +0800

    Inject TiDB同LightRAG storage when needed

--- lightrag/kg/tidb_impl.py
+++ lightrag/kg/tidb_impl.py
@@ -102,6 +102,8 @@ class TiDB:
 @dataclass
 class TiDBKVStorage(BaseKVStorage):
     # should pass db object to self.db
+    db: TiDB = None
+
     def __post_init__(self):
         self._data = {}
         self._max_batch_size = self.global_config["embedding_batch_num"]
@@ -208,6 +210,8 @@ class TiDBKVStorage(BaseKVStorage):
 
 @dataclass
 class TiDBVectorDBStorage(BaseVectorStorage):
+    # should pass db object to self.db
+    db: TiDB = None
     cosine_better_than_threshold: float = float(os.getenv("COSINE_THRESHOLD", "0.2"))
 
     def __post_init__(self):
@@ -329,6 +333,9 @@ class TiDBVectorDBStorage(BaseVectorStorage):
 
 @dataclass
 class TiDBGraphStorage(BaseGraphStorage):
+    # should pass db object to self.db
+    db: TiDB = None
+
     def __post_init__(self):
         self._max_batch_size = self.global_config["embedding_batch_num"]
 
