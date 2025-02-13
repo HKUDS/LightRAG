dcbfef8e55173c1c3eb81309839d18accd84369f
ArnoChen <arnochen101@gmail.com>
2025-02-14 03:00:56 +0800

    fix configuration errors of milvus

--- lightrag/kg/milvus_impl.py
+++ lightrag/kg/milvus_impl.py
@@ -32,8 +32,8 @@ class MilvusVectorDBStorage(BaseVectorStorage):
         )
 
     def __post_init__(self):
-        config = self.global_config.get("vector_db_storage_cls_kwargs", {})
-        cosine_threshold = config.get("cosine_better_than_threshold")
+        kwargs = self.global_config.get("vector_db_storage_cls_kwargs", {})
+        cosine_threshold = kwargs.get("cosine_better_than_threshold")
         if cosine_threshold is None:
             raise ValueError(
                 "cosine_better_than_threshold must be specified in vector_db_storage_cls_kwargs"
