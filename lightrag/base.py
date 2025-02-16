4e354451b7a0087f20cb9311708b384cadc23d89
yangdx <yangdx@znipower.com>
2025-02-16 20:05:45 +0800

    Fix load_dotenv() missing problem for base.py

--- lightrag/base.py
+++ lightrag/base.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from dataclasses import dataclass, field
 from enum import Enum
 from typing import (
@@ -9,12 +10,11 @@ from typing import (
     TypeVar,
     Union,
 )
-
 import numpy as np
-
 from .utils import EmbeddingFunc
 from .types import KnowledgeGraph
 
+load_dotenv()
 
 class TextChunkSchema(TypedDict):
     tokens: int
