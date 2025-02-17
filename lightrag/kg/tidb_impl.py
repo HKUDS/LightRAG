66c4b01fdd7d57b0e8853151bdac378494b37484
Yannick Stephan <stephan.yannick@me.com>
2025-02-17 23:16:17 +0100

    remove drops unused

--- lightrag/kg/tidb_impl.py
+++ lightrag/kg/tidb_impl.py
@@ -214,10 +214,6 @@ class TiDBKVStorage(BaseKVStorage):
         # Ti handles persistence automatically
         pass
 
-    async def drop(self) -> None:
-        raise NotImplementedError
-
-
 @final
 @dataclass
 class TiDBVectorDBStorage(BaseVectorStorage):
