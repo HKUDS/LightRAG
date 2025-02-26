d79a9d7acc5ad8f62e6c45dacf180f9b323218c6
Zhichun Wu <zhicwu@gmail.com>
2025-02-26 23:04:21 +0800

    consistent format

--- lightrag/prompt.py
+++ lightrag/prompt.py
@@ -47,8 +47,9 @@ Format the content-level key words as ("content_keywords"{tuple_delimiter}<high_
 #############################
 ---Real Data---
 ######################
-Entity_types: {entity_types}
-Text: {input_text}
+Entity_types: [{entity_types}]
+Text:
+{input_text}
 ######################
 Output:"""
 
