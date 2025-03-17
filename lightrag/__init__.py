d7c0de3d7c43d04bee500ae58346db9404a9ee21
bennoloeffler <benno.loeffler@gmx.de>
2025-03-17 23:00:56 +0100

    correct build

--- lightrag/__init__.py
+++ lightrag/__init__.py
@@ -2,4 +2,5 @@ from .lightrag import LightRAG as LightRAG, QueryParam as QueryParam
 
 __version__ = "1.2.6"
 __author__ = "Zirui Guo"
-__url__ = "https://github.com/HKUDS/LightRAG"
+__url__ = "https://github.com/bennoloeffler/LightRAG_BEL_FORK.git"
+#__url__ = "https://github.com/HKUDS/LightRAG"
