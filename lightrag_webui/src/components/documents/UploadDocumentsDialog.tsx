53b6b95e9e0072222782f1544d01fa596672d847
yangdx <yangdx@znipower.com>
2025-03-26 19:52:20 +0800

    Fix linting

--- lightrag_webui/src/components/documents/UploadDocumentsDialog.tsx
+++ lightrag_webui/src/components/documents/UploadDocumentsDialog.tsx
@@ -54,7 +54,7 @@ export default function UploadDocumentsDialog() {
         // setOpen(false)
       }
     },
-    [setIsUploading, setProgresses]
+    [setIsUploading, setProgresses, t]
   )
 
   return (
