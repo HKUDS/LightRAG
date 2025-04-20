697401fdc31d47209c1b92e67f44f63129967dbd
yangdx <yangdx@znipower.com>
2025-04-20 21:39:51 +0800

    Change OpenAI demo to asyc

--- examples/lightrag_openai_demo.py
+++ examples/lightrag_openai_demo.py
@@ -24,41 +24,41 @@ async def initialize_rag():
     return rag
 
 
-def main():
+async def main():
     # Initialize RAG instance
-    rag = asyncio.run(initialize_rag())
+    rag = await initialize_rag()
 
     with open("./book.txt", "r", encoding="utf-8") as f:
-        rag.insert(f.read())
+        await rag.ainsert(f.read())
 
     # Perform naive search
     print(
-        rag.query(
+        await rag.aquery(
             "What are the top themes in this story?", param=QueryParam(mode="naive")
         )
     )
 
     # Perform local search
     print(
-        rag.query(
+        await rag.aquery(
             "What are the top themes in this story?", param=QueryParam(mode="local")
         )
     )
 
     # Perform global search
     print(
-        rag.query(
+        await rag.aquery(
             "What are the top themes in this story?", param=QueryParam(mode="global")
         )
     )
 
     # Perform hybrid search
     print(
-        rag.query(
+        await rag.aquery(
             "What are the top themes in this story?", param=QueryParam(mode="hybrid")
         )
     )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
