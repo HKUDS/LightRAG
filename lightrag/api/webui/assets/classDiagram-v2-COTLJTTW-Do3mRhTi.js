cc67c6eb8dffeead5a1e42ee51be5203fdeb7b71
yangdx <yangdx@znipower.com>
2025-04-22 09:23:40 +0800

    Update webui assets

---
+++ lightrag/api/webui/assets/classDiagram-v2-COTLJTTW-Do3mRhTi.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-Dvvy5gG_.js";import{_ as l}from"./index-CQkzNkCW.js";import"./chunk-RZ5BOZE2-BCG5LCJ0.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
