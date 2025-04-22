3b8959051dfd768862233696dc70a7f3da88bbc3
yangdx <yangdx@znipower.com>
2025-04-22 12:11:24 +0800

    Fix i18n translate error

--- lightrag/api/webui/assets/classDiagram-GIVACNV2-sIQ8VIsV.js
+++
@@ -1,1 +0,0 @@
-import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-Bn26p3Tg.js";import{_ as l}from"./index-D_eVD4HC.js";import"./chunk-RZ5BOZE2-DvBnAms2.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
---
+++ lightrag/api/webui/assets/classDiagram-v2-COTLJTTW-acOnbCpO.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-yDVppOXb.js";import{_ as l}from"./index-BPSbheeW.js";import"./chunk-RZ5BOZE2-CrQI9BKy.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
