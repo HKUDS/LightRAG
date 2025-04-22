bce2eb60eabcf010b7e947e5cc8d2c1f0b5ec8f0
yangdx <yangdx@znipower.com>
2025-04-22 16:16:14 +0800

    Fix layout problem of QuerySettings for WebUI

---
+++ lightrag/api/webui/assets/classDiagram-GIVACNV2-B39dd-gN.js
@@ -0,0 +1,1 @@
+import{s as r,c as s,a as e,C as t}from"./chunk-A2AXSNBT-0gMzkJaE.js";import{_ as l}from"./index-CCH90ug1.js";import"./chunk-RZ5BOZE2-DEeOEMCb.js";var m={parser:e,get db(){return new t},renderer:s,styles:r,init:l(a=>{a.class||(a.class={}),a.class.arrowMarkerAbsolute=a.arrowMarkerAbsolute},"init")};export{m as diagram};
