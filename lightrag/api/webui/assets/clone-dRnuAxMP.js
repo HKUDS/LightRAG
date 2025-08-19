1f86543772fdd070b4b2e304824efaf8a993c8c8
yangdx <gzdaniel@me.com>
2025-08-19 16:23:05 +0800

    Update i18n translation and webui assets

--- lightrag/api/webui/assets/clone-dRnuAxMP.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-C9YcK3GC.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- lightrag_webui/src/lib/constants.ts
+++ lightrag_webui/src/lib/constants.ts
@@ -64,8 +64,6 @@ export const supportedFileTypes = {
     '.less'
   ],
   'application/pdf': ['.pdf'],
-  'application/msword': ['.doc'],
-  'application/vnd.ms-excel': ['.xls'],
   'application/vnd.openxmlformats-officedocument.wordprocessingml.document': ['.docx'],
   'application/vnd.openxmlformats-officedocument.presentationml.presentation': ['.pptx'],
   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': ['.xlsx']
