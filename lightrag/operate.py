28e07c89f9eda65d0b7d1b8b9da77b22ca8cbeef
yangdx <gzdaniel@me.com>
2025-08-27 12:35:51 +0800

    Fix linting

--- lightrag/operate.py
+++ lightrag/operate.py
@@ -270,7 +270,7 @@ async def _summarize_descriptions(
     use_llm_func = partial(use_llm_func, _priority=8)
 
     language = global_config["addon_params"].get(
-        "language", DEFAULT_SUMMARY_LANGUAGE]
+        "language", DEFAULT_SUMMARY_LANGUAGE
     )
 
     summary_length_recommended = global_config["summary_length_recommended"]
