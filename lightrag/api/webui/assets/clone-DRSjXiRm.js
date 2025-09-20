9e0dabc34d2220fe8cfcdbe10c0278f08e0cd12f
yangdx <gzdaniel@me.com>
2025-09-20 23:31:00 +0800

    Update webui assets

---
+++ lightrag/api/webui/assets/clone-DRSjXiRm.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-BghZ2pUG.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- lightrag/api/webui/assets/clone-bEtF6mbL.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-CiQkfnAT.js";var e=4;function a(o){return r(o,e)}export{a as c};
