b13209e241c32f4e18673e36188cfed03b17cf96
yangdx <gzdaniel@me.com>
2025-09-22 03:04:06 +0800

    Update webui assets

--- lightrag/api/webui/assets/clone-DRSjXiRm.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-BghZ2pUG.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ lightrag/api/webui/assets/clone-oMR_cG9e.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-2h-DWqJv.js";var e=4;function a(o){return r(o,e)}export{a as c};
