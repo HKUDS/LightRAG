dde728a32f19d6d83f1e49e9dc67a98623815715
yangdx <gzdaniel@me.com>
2025-10-05 09:25:57 +0800

    Bump core version to 1.5.0 and API to 0236

--- lightrag/__init__.py
+++ lightrag/__init__.py
@@ -1,5 +1,5 @@
 from .lightrag import LightRAG as LightRAG, QueryParam as QueryParam
 
-__version__ = "1.4.9.1"
+__version__ = "1.5.0"
 __author__ = "Zirui Guo"
 __url__ = "https://github.com/HKUDS/LightRAG"
