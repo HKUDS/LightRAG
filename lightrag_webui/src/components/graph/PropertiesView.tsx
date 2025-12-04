5b207db37ef7526715cc2c8f3cb157b6e1734580
Raphaël MANSUY <raphael.mansuy@gmail.com>
2025-12-04 19:18:36 +0800

    cherry-pick 637b850e

--- lightrag_webui/src/components/graph/PropertiesView.tsx
+++ lightrag_webui/src/components/graph/PropertiesView.tsx
@@ -222,7 +222,7 @@ const PropertyRow = ({
 
   // If this is source_id field and truncate info exists, append it to the tooltip
   if (name === 'source_id' && truncate) {
-    formattedTooltip += `\n(Truncation-${truncate})`
+    formattedTooltip += `\n(${truncate} truncated)`
   }
 
   // Use EditablePropertyRow for editable fields (description, entity_id and keywords)
